8bb7b4c6c3fc5888b8425afcde8eebd3bd71096c
sanggyeol1 <sanggyeol1@gachon.ac.kr>
2023-07-24 22:29:56 +0900

    댓글db에 저장기능, 조회기능 연습

--- app/detail/[id]/Comment.js
+++ app/detail/[id]/Comment.js
@@ -1,26 +1,33 @@
 'use client'
-import {useState} from "react"
+import {useEffect, useState} from "react"
 
 
-export default function Comment(){
+export default function Comment(props){
     let [comment, setComment] = useState('')
 
+    //client component로드 시 서버에 데이터 요청하려면 useEffect사용
+    useEffect(()=>{//쓸데없는 코드 보관함, 보통 ajax나 타이머를 넣음
+        fetch()
+    },[])//대괄호 넣으면 comment컴포넌트가 로드될 때 1회만 실행됨
+    
 
     return(
         <div>
-            <div>댓글목록보여줄부분</div>
-            <input onChange={(e)=>{
-                setComment(e.target.value) 
-            }}/>
+            <div>
+
+            </div>
+
+
+            <input onChange={(e)=>{ setComment(e.target.value) }}/>
             {/* 폼태그를 사용하면 새로고침이 되기때문에 패치를 쓴다. */}
             <button onClick={()=>{
-                console.log(comment)
-                fetch('URL', {
+                fetch('/api/comment/new', {
                     method : 'POST', 
                     body : JSON.stringify({
-                        content : comment,
-                       
+                        comment : comment,
+                        _id : props.result._id
                     })
+        
                 })
             }}>댓글전송</button>
         </div>
--- app/detail/[id]/page.js
+++ app/detail/[id]/page.js
@@ -3,9 +3,12 @@
 import { connectDB } from "@/util/database"
 import { ObjectId } from "mongodb"
 import  Comment  from './Comment.js'
-import { getServerSession } from "next-auth";
+
+
+
 
 export default async function Detail(props){//props : 유저가 다이나믹 라우트에 입력한 값을출력가능
+   
 
     const db = (await connectDB).db("forum")//db에접속
     let result = await db.collection('post').findOne({ _id: new 
@@ -17,7 +20,7 @@ export default async function Detail(props){//props : 유저가 다이나믹 라
             <h4>상세페이지</h4>
             <h4>{result.title}</h4>
             <p>{result.content}</p>
-            <Comment/>
+            <Comment result={result}/>
         </div>
     )
 }
